49a2b48c9bc2958fb4fa53666c89f2e9a70328cf
Ajai Kuruppath <ajaymenon.k@gmail.com>
2020-02-11 00:34:38 +0100

    Fix application bootstrap class.
    1. Add annotations to scan the repos
    2. Scan the controller and domain classes

--- src/main/java/com/afkl/travel/exercise/Application.java
+++ src/main/java/com/afkl/travel/exercise/Application.java
@@ -3,13 +3,17 @@ package com.afkl.travel.exercise;
 import javax.sql.DataSource;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
-@EnableAutoConfiguration
+@SpringBootApplication
+@ComponentScan(basePackages = "com.afkl.travel.exercise")
+@EnableJpaRepositories("com.afkl.travel.exercise.repositories")
 public class Application {
 
     @Bean
