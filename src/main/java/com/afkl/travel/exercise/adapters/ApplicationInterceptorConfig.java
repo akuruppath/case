eaf42eda4b7f2a148e83102c5687d739821521c3
Ajai Kuruppath <ajaymenon.k@gmail.com>
2020-02-11 04:01:25 +0100

    Make it a component instead of a configuration

--- src/main/java/com/afkl/travel/exercise/adapters/ApplicationInterceptorConfig.java
+++ src/main/java/com/afkl/travel/exercise/adapters/ApplicationInterceptorConfig.java
@@ -1,11 +1,11 @@
 package com.afkl.travel.exercise.adapters;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Component
 public class ApplicationInterceptorConfig implements WebMvcConfigurer  {
 
 	private final RequestInterceptor requestInterceptor;
