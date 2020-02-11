d41b5ede56ad2a951902a05c8d11352c12ad9ba4
Ajai Kuruppath <ajaymenon.k@gmail.com>
2020-02-11 11:01:34 +0100

    Log request URL and response status with the correlation id.

--- src/main/java/com/afkl/travel/exercise/adapters/RequestInterceptor.java
+++ src/main/java/com/afkl/travel/exercise/adapters/RequestInterceptor.java
@@ -6,28 +6,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 @Component
 public class RequestInterceptor implements HandlerInterceptor {
 
 	private static final String CORRELATION_ID_HEADER_NAME = "X-Correlation-Id";
 	private static final String CORRELATION_ID_LOG_VAR_NAME = "correlationId";
+	private static final Logger LOGGER = LoggerFactory.getLogger(RequestInterceptor.class);
 
 	@Override
 	public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler)
 			throws Exception {
 		final String correlationId = getCorrelationIdFromHeader(request);
 		MDC.put(CORRELATION_ID_LOG_VAR_NAME, correlationId);
+		LOGGER.info("Request URL: {}", request.getRequestURL());
 		return true;
 	}
 
 	@Override
 	public void afterCompletion(final HttpServletRequest request, final HttpServletResponse response,
 			final Object handler, final Exception ex) throws Exception {
+		if (response != null) {
+			LOGGER.info("Response : {}", response.getStatus());
+		}
 		MDC.remove(CORRELATION_ID_LOG_VAR_NAME);
 	}
 
