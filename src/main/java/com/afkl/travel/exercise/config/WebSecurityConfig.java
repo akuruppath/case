911daf92a9bda76dfeeacf1a2406463c15f7d924
Ajai Kuruppath <ajaymenon.k@gmail.com>
2020-02-13 18:59:31 +0100

    Populate property values.

--- src/main/java/com/afkl/travel/exercise/config/WebSecurityConfig.java
+++ src/main/java/com/afkl/travel/exercise/config/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.afkl.travel.exercise.config;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -13,37 +16,44 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import com.afkl.travel.exercise.utils.constants.Constants;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
+	private static final String[] ALLOWED_URLS = new String[] { "/, /**" };
+
+	private final Map<String, String> locationsMap;
+	private final Map<String, String> actuatorMap;
+
+	@Autowired
+	public WebSecurityConfig(ApplicationConfiguration appConfig) {
+		this.locationsMap = appConfig.getLocations();
+		this.actuatorMap = appConfig.getActuator();
+	}
+
 	@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests() 
-                .antMatchers("/, /**").permitAll()
-                .antMatchers("/locations/**").hasRole("USER")
-                .antMatchers("/actuator/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers(ALLOWED_URLS).permitAll()
+				.antMatchers(locationsMap.get(Constants.URL) + "/**").hasRole(locationsMap.get(Constants.ROLE))
+				.antMatchers(actuatorMap.get(Constants.URL) + "/**").hasRole(actuatorMap.get(Constants.ROLE))
+				.anyRequest().authenticated().and().httpBasic().and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
 	@Bean
 	@Override
 	public UserDetailsService userDetailsService() {
 
-		UserDetails theUser = User.withUsername("someuser")
-				.passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder()::encode).password("psw")
-				.roles("USER").build();
+		UserDetails theUser = User.withUsername(locationsMap.get(Constants.USER_NAME))
+				.passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder()::encode)
+				.password(locationsMap.get(Constants.PASSWORD)).roles(locationsMap.get(Constants.ROLE)).build();
 
-		UserDetails theManager = User.withUsername("ops")
-				.passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder()::encode).password("psw")
-				.roles("ADMIN").build();
+		UserDetails theManager = User.withUsername(actuatorMap.get(Constants.USER_NAME))
+				.passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder()::encode)
+				.password(actuatorMap.get(Constants.PASSWORD)).roles(actuatorMap.get(Constants.ROLE)).build();
 
 		InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
 
