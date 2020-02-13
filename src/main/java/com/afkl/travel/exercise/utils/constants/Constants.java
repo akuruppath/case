d79751534fb6bbd4f03abefaa71a12963e34fbd3
Ajai Kuruppath <ajaymenon.k@gmail.com>
2020-02-13 18:47:44 +0100

    Introduce constants.

---
+++ src/main/java/com/afkl/travel/exercise/utils/constants/Constants.java
@@ -0,0 +1,19 @@
+package com.afkl.travel.exercise.utils.constants;
+
+public final class Constants {
+
+	private Constants() {
+		// EMPTY
+	}
+
+	public static final String URL = "url";
+	public static final String USER_NAME = "username";
+	public static final String PASSWORD = "password";
+	public static final String ROLE = "role";
+	
+	public static final String LOCAL_HOST = "localhost";
+	public static final String PROTOCOL = "http";
+	
+	public static final String MIME_TYPE = "application/json;charset=UTF-8";
+
+}
